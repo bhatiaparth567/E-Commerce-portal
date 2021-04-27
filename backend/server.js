ec13da971c2b8de03dcec65b53d74a52e461a6eb
Sumant <sumant@Sumants-MacBook-Air.local>
2021-04-28 00:59:08 +0530

    production changes

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,11 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  console.log(path.join(__dirname, "../frontend/build"));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+}
 app.use("/", require("./routes/index"));
 
 app.use(errorMiddleware.notFound);
