3bfcd99ebc759f36ef47c7b91db39525cef4683c
Sumant <sumant@Sumants-MacBook-Air.local>
2021-04-28 01:32:42 +0530

    bug fixes

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,16 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use("/", require("./routes/index"));
 
 if (process.env.NODE_ENV === "production") {
   console.log(path.join(__dirname, "../frontend/build"));
   app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
 }
-app.use("/", require("./routes/index"));
 
 app.use(errorMiddleware.notFound);
 app.use(errorMiddleware.errorHandler);
